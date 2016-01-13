434cd95e3f3d52b7f25c851a58283407bf99f690
atompkin <atompkin@londo.stetson.edu>
2016-01-13 18:16:24 -0500

    Added questions to start game

--- numberwang.cpp
+++ numberwang.cpp
@@ -1,1 +1,35 @@
-....
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string name1;
+	string name2;
+	int round;
+	int temp;i
+       cout << "Hello, and welcome to Numberwang, the maths quiz that simply everyone is talking about!" << endl;
+
+cout << "What is player 1's name? ";
+cin >> name1;
+cout << endl;
+cout << "what is player 2's name? ";
+cin >> name2;
+cout << endl; 
+ cout << "How many rounds? ";
+ cin >> round;
+ cout << endl;
+cout << name1 << ": ";
+cin >> temp;
+
+ cout << "end of script";
+ return 0;
+}
+
+int numberwang(int n)
+{
+	int numberwang = 0;
+	if(n == numberwang)
+	{
+		cout << endl << "That's Numberwang!" << endl;
+	}
+}
