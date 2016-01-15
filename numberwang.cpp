bcca839755b0c0513dbbe192efc962d1025fd862
atompkin <atompkin@londo.stetson.edu>
2016-01-15 14:19:10 -0500

    added numberwang rules and final score

--- numberwang.cpp
+++ numberwang.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
+
+bool numberwang(int n)
+{
+	if(n == 0)
+	{
+		cout << endl << "That's Numberwang!" << endl;
+		return true;
+	}
+	return false;
+}
 
 int main()
 {
+	bool player = true;
 	string name1;
 	string name2;
 	int round;
-	int temp;i
+	int temp;
+	int time = rand() % 5;
        cout << "Hello, and welcome to Numberwang, the maths quiz that simply everyone is talking about!" << endl;
 
 cout << "What is player 1's name? ";
@@ -18,18 +31,34 @@ cout << endl;
  cout << "How many rounds? ";
  cin >> round;
  cout << endl;
-cout << name1 << ": ";
-cin >> temp;
+ cout << "Well, if you're ready, let's play Numberwang!" << endl;
 
- cout << "end of script";
+while(round>0)
+{
+	if(player)
+	{
+		cout << name1 << ": ";
+		cin >> temp;
+		player = false;
+	}
+       	else
+       	{
+		cout << name2 << ": ";
+		cin >> temp;
+		player = true;
+	}
+
+	if(numberwang(time))
+	{
+		round --;
+		time = rand() % 5 + 1;
+	}
+	time --;
+}
+
+cout << "Final scores: " << name2 << " pulls ahead with " << rand() % 210 + 30 << ", ";
+cout << "and " << name1 << " finishes with " << rand() % 25 + 2 << "." << endl;
  return 0;
 }
 
-int numberwang(int n)
-{
-	int numberwang = 0;
-	if(n == numberwang)
-	{
-		cout << endl << "That's Numberwang!" << endl;
-	}
-}
+
